0533eaf69a4a066001628f702575a89bf75099f7
Evgenii Klimov <disturh@gmail.com>
2018-04-02 17:57:56 +0300

    breaking changes. Function "done" must be the first argument

--- flow.js
+++ flow.js
@@ -1,13 +1,14 @@
+'use strict'
+const isBrowser = typeof window !== 'undefined' && ({}).toString.call(window) === '[object Window]';
+// const isNode = typeof global !== "undefined" && ({}).toString.call(global) === '[object global]';
+
 const defSettings = {
     isLogging: true,
     isErrors: true,
-
-    logStyle: '\x1b[32m%s\x1b[0m',
-    errStyle: '\x1b[31m%s\x1b[0m',
-
+    logStyle: isBrowser ? 'color: green' : '\x1b[32m%s\x1b[0m',
+    errStyle: isBrowser ? 'color: red' : '\x1b[31m%s\x1b[0m',
     logName: '--> ',
 }
-
 
 module.exports = class Flow {
     constructor (params) {
@@ -21,13 +22,14 @@ module.exports = class Flow {
 
     done (stepName, dataForNextStep) {
         const message = `${this.settings.logName}${stepName}`;
+        const doneCb = this.done.bind(this);
         let stepHandler = this.steps[stepName];
 
         this.LOG (message); // Previous step is finished
 
         if (stepHandler) {
             if (typeof stepHandler === 'function') {
-                stepHandler (dataForNextStep);
+                stepHandler (doneCb, dataForNextStep);
                 return;
             }
             if (stepHandler instanceof Array) {
@@ -37,11 +39,7 @@ module.exports = class Flow {
                         return false;
                     }
                 }
-                stepHandler.forEach (func => func (dataForNextStep));
-                return;
-            } 
-            if (typeof stepHandler.fn === 'function') {
-                stepHandler.fn (dataForNextStep);
+                stepHandler.forEach (func => func (doneCb, dataForNextStep));
                 return;
             }
 
@@ -59,10 +57,16 @@ module.exports = class Flow {
     // Let's imagine these are private methods
     ERR (msg) {
         const { isErrors, errStyle } = this.settings;
-        isErrors && console.error (errStyle, msg);
+        if (isErrors) 
+            if (!errStyle) console.error (msg);
+            else if (isBrowser) console.error ('%c'+ msg, errStyle);
+            else console.error (errStyle, msg);
     }
     LOG (msg) {
         const { isLogging, logStyle } = this.settings;
-        isLogging && console.log (logStyle, msg);
+        if (isLogging) 
+            if (!logStyle) console.log (msg);
+            else if (isBrowser) console.log ('%c'+ msg, logStyle);
+            else console.log (logStyle, msg);
     }
 }
