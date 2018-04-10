6e269ac376e7e0899c1d2e59525076e0b68417a6
Evgenii Klimov <disturh@gmail.com>
2018-04-10 17:20:35 +0300

    The final changes

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-Flow
-====
+# Flow
 
 "**Flow**" is a simple way to write readable asynchronous code using just callbacks.
 
+**Warning**: this software got the stable version and will not be supported anymore.
+The author decided to replace this lib by another, more powerful one ([brief-async](https://github.com/klimcode/brief-async)).
+
+This lib is used by [Notekeeper](https://github.com/klimcode/notekeeper) which is also discontinued.
+
 ## Usage
+
 ```js
 const Flow = require('flow-code-description');
-const mainFlow = new Flow(true);
-
+const mainFlow = new Flow();
 mainFlow.steps = {
     'start': getConfig,
     'config is OK': checkDataBase,
@@ -15,41 +19,42 @@ mainFlow.steps = {
     'interface is ready': renderInterface,
     // ...
 }
-mainFlow.done('start');
+mainFlow.start();
 
-function getConfig() {
+function getConfig(done) {
     // ...
 ```
 
-`mainFlow.steps` is a list of steps of your app execution.
-The App execution starts by `mainFlow.done('start')`. A function `done` summarizes the current step.
+`mainFlow.steps` is a list of steps of your app's execution.
+The App's execution starts by `mainFlow.start()`. A function `done` summarizes the current step.
 
-The meaning of `mainFlow.done('start')` is: "the program has finished a step called "start". Then a function specified in `mainFlow.steps` must be executed. This function is `getConfig`".
+A result of the finished step may be specified in the second argument of a `done` callback:
 
-A result of the finished step may be specified in the second argument:
 ```js
-function getInterface() {
+function getInterface(done) {
     let interface = {};
     // some actions ...
-    mainFlow.done('interface is ready', interface);
+    done('interface is ready', interface);
 }
 ```
 
 This result will be transmitted to the next function (it's `renderInterface` in this case):
+
 ```js
-function renderInterface(interface) {
+function renderInterface(done, interface) {
     console.log(interface);
     // ...
-    mainFlow.done('interface is rendered');
+    done('interface is rendered');
 }
 ```
 
 .
 
-
 ## Steps
+
 A finished step may execute several functions asynchronously:
-```
+
+```js
 mainFlow.steps = {
     'start': getConfig,
     'config is OK': [checkDataBase, getInterface]
@@ -57,34 +62,40 @@ mainFlow.steps = {
 }
 ```
 
-You may check if a step finished:
-```
-if (mainFlow['database is OK'].isDone) { // getting data from the base... }
-```
-
 .
 
 ## Asynchronous race
+
 Beware of setting one function as a result of several different steps:
-```
+
+```js
 mainFlow.steps = {
     'file 1 is read': mixFiles,
     'file 2 is read': mixFiles,
 }
 ```
-A function may depend on data received from different asynchronous sources `mixFiles (file1, file2)`. Current version of **Flow** does not handle this case. You may store that data and check it manually.
+
+A function may depend on data received from different asynchronous sources `mixFiles (file1, file2)`. **Flow** does not handle this case. You may store that data somewhere and check it manually.
 
 .
 
-
 ## A Constructor parameters
+
 `Flow` accepts an Object with settings as an argument.
+
 ```js
 const defParams = {
     isLogging: true, // steps logging is turned on
     isErrors: true,  // errors logging is turned on
     logStyle: '\x1b[32m%s\x1b[0m', // green font color (Node.js)
     errStyle: '\x1b[31m%s\x1b[0m', // red font color (Node.js)
+    logName: '--> ' // a prefix before each log message
 }
-const mainFlow = new Flow (defParams);
+const mainFlow = new Flow(defParams);
 ```
+
+## Methods
+
+`.done(stepName, results)` -- marks the current step as finished and calls functions of the next step.
+
+`.start(initialData)` -- starts the flow execution (tha same as `.done('start')`).
--- flow.js
+++ flow.js
@@ -1,72 +1,79 @@
-'use strict'
+/* eslint no-console: "off" */
 const isBrowser = typeof window !== 'undefined' && ({}).toString.call(window) === '[object Window]';
 // const isNode = typeof global !== "undefined" && ({}).toString.call(global) === '[object global]';
 
 const defSettings = {
-    isLogging: true,
-    isErrors: true,
-    logStyle: isBrowser ? 'color: green' : '\x1b[32m%s\x1b[0m',
-    errStyle: isBrowser ? 'color: red' : '\x1b[31m%s\x1b[0m',
-    logName: '--> ',
-}
+  isLogging: true,
+  isErrors: true,
+  logStyle: isBrowser ? 'color: green' : '\x1b[32m%s\x1b[0m',
+  errStyle: isBrowser ? 'color: red' : '\x1b[31m%s\x1b[0m',
+  logName: '--> ',
+};
 
 module.exports = class Flow {
-    constructor (params) {
-        this.steps = params.steps || {};
-        this.settings = params.settings;
-        for (let key in defSettings) {
-            if (this.settings[key] === void 0) this.settings[key] = defSettings[key];
+  constructor(params) {
+    if (params) {
+      this.steps = params.steps || {};
+      this.settings = params.settings || {};
+    } else {
+      this.settings = {};
+    }
+    Object.keys(defSettings).forEach((key) => {
+      if (this.settings[key] === undefined) {
+        this.settings[key] = defSettings[key];
+      }
+    });
+    return this;
+  }
+
+  done(stepName, dataForNextStep) {
+    const message = `${this.settings.logName}${stepName}`;
+    const doneCb = this.done.bind(this);
+    const stepHandler = this.steps[stepName];
+
+    this.LOG(message); // Previous step is finished
+
+    if (stepHandler) {
+      if (typeof stepHandler === 'function') {
+        stepHandler(doneCb, dataForNextStep);
+        return;
+      }
+      if (stepHandler instanceof Array) {
+        if (stepHandler.some(func => typeof func !== 'function')) {
+          this.ERR('FLOW TYPE ERROR: handler must be a function');
+          return false;
         }
-        return this;
-    }
+        stepHandler.forEach(func => func(doneCb, dataForNextStep));
+        return;
+      }
 
-    done (stepName, dataForNextStep) {
-        const message = `${this.settings.logName}${stepName}`;
-        const doneCb = this.done.bind(this);
-        let stepHandler = this.steps[stepName];
-
-        this.LOG (message); // Previous step is finished
-
-        if (stepHandler) {
-            if (typeof stepHandler === 'function') {
-                stepHandler (doneCb, dataForNextStep);
-                return;
-            }
-            if (stepHandler instanceof Array) {
-                for (let func of stepHandler) {
-                    if (typeof func !== 'function') {
-                        this.ERR (`FLOW TYPE ERROR: handler: "${func}" must be a function`)
-                        return false;
-                    }
-                }
-                stepHandler.forEach (func => func (doneCb, dataForNextStep));
-                return;
-            }
-
-            this.ERR (`FLOW TYPE ERROR: wrong type of a handler: "${stepHandler}"`);
-            return false;
-        } else {
-            this.ERR (`FLOW ERROR: undefined handler`);
-            return false;
-        }
+      this.ERR(`FLOW TYPE ERROR: wrong type of a handler: "${stepHandler}"`);
+      return false;
+    } else {
+      this.ERR(`FLOW ERROR: undefined handler for step: "${stepName}"`);
+      return false;
     }
-    start (dataForNextStep) {
-        this.done ('start');
-    }
+  }
+  start(dataForNextStep) {
+    this.done('start', dataForNextStep);
+  }
 
-    // Let's imagine these are private methods
-    ERR (msg) {
-        const { isErrors, errStyle } = this.settings;
-        if (isErrors) 
-            if (!errStyle) console.error (msg);
-            else if (isBrowser) console.error ('%c'+ msg, errStyle);
-            else console.error (errStyle, msg);
+
+  // Let's imagine these are private methods
+  ERR(msg) {
+    const { isErrors, errStyle } = this.settings;
+    if (isErrors) {
+      if (!errStyle) console.error(msg);
+      else if (isBrowser) console.error(`%c${msg}`, errStyle);
+      else console.error(errStyle, msg);
     }
-    LOG (msg) {
-        const { isLogging, logStyle } = this.settings;
-        if (isLogging) 
-            if (!logStyle) console.log (msg);
-            else if (isBrowser) console.log ('%c'+ msg, logStyle);
-            else console.log (logStyle, msg);
+  }
+  LOG(msg) {
+    const { isLogging, logStyle } = this.settings;
+    if (isLogging) {
+      if (!logStyle) console.log(msg);
+      else if (isBrowser) console.log(`%c${msg}`, logStyle);
+      else console.log(logStyle, msg);
     }
-}
+  }
+};
